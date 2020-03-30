39b91f61f17bda45409b0982983813c742481101
Emmanuel Maneswa <emmanuelmaneswa@gmail.com>
2020-03-30 08:14:45 +0300

    Added files that will contain the steganography implementations

---
+++ src/steganography.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file steganography.cpp
+ * @author Emmanuel Maneswa (emmanuelmaneswa@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-03-30
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
