aa76bdbf526248c5946f30da532a31fe6005bdda
Emmanuel Maneswa <emmanuelmaneswa@gmail.com>
2020-04-07 15:42:12 +0300

    Implemented steganographic embedding

--- include/steganography.hpp
+++ include/steganography.hpp
@@ -4,7 +4,7 @@
  * @brief This header file contains the class definition for image steganography.
  *        Class implementation path: "../src/steganography.cpp".
  * @version 0.1
- * @date 2020-04-01
+ * @date 2020-04-07
  * 
  * @copyright Copyright (c) 2020
  * 
@@ -22,8 +22,116 @@
  */
 class Steganography{
 
-        std::string secret_info_, key_, img_path_;
+        std::string secret_info_, key_, key_hash_, img_path_;
         cv::Mat img_;
+
+        /**
+         * @brief Loads image from the path given (img_path_).
+         * 
+         */
+        void LoadImage();
+        
+        /**
+         * @brief Saves the stego image.
+         * 
+         * @param stego_image_path 
+         */
+        void SaveStegoImage(std::string stego_img_path);
+
+        /**
+         * @brief Calculates the number of secret information bits and returns it as bits.
+         * 
+         * @return std::string 
+         */
+        std::string HiddenBits(size_t size);
+
+        /**
+         * @brief checks if the secret infomation, hashed key and the legth of hidden info can be embedded into the cover image.
+         * 
+         * @return true 
+         * @return false 
+         */
+        bool IfStorable(std::string number_of_bits_hidden)const;
+
+        /**
+         * @brief Checks if value is an even number.
+         * 
+         * @param color_value 
+         * @return true 
+         * @return false 
+         */
+        bool IsEven(unsigned char color_shade)const;
+
+        /**
+         * @brief Checks if value is an odd number.
+         * 
+         * @param color_value 
+         * @return true 
+         * @return false 
+         */
+        bool IsOdd(unsigned char color_shade)const;
+
+        /**
+         * @brief Exclusive Or of of two bits of different data types.
+         * 
+         * @param secret_bit 
+         * @param green_lsb 
+         * @return true 
+         * @return false 
+         */
+        bool XOR(char secret_bit, bool lsb)const;
+
+        /**
+         * @brief Circulates the key making ith behave like a circular string.
+         * 
+         */
+        void CirculateKey();
+
+        /**
+         * @brief Embeds secret information bit in the LSB of blue.
+         * 
+         * @param pixel 
+         * @param index 
+         * @return cv::Vec3b 
+         */
+        cv::Vec3b EmbedInBlue(cv::Vec3b pixel, size_t index);
+
+        /**
+         * @brief Embeds secret information bit in the LSB of green.
+         * 
+         * @param pixel 
+         * @param index 
+         * @return cv::Vec3b 
+         */
+        cv::Vec3b EmbedInGreen(cv::Vec3b pixel, size_t index);
+
+        /**
+         * @brief Embeds secret information bit in the LSB of red.
+         * 
+         * @param pixel 
+         * @param index 
+         * @return cv::Vec3b 
+         */
+        cv::Vec3b EmbedInRed(cv::Vec3b pixel, size_t index);
+        
+        /**
+         * @brief Embeds the length of the secret information bits into the cover image.
+         * 
+         * @param number_of_bits_hidden 
+         */
+        void EmbedLength(std::string number_of_bits_hidden);
+
+        /**
+         * @brief Embeds the hashed key bits into the cover image.
+         * 
+         */
+        void EmbedKeyHash();
+
+        /**
+         * @brief Embeds the secret key bits into the cover image.
+         * 
+         */
+        void EmbedSecretInfo();
 
     public:
 
@@ -34,7 +142,7 @@ class Steganography{
         Steganography() = delete;
 
         /**
-         * @brief Construct a new Steganography object.
+         * @brief Construct a new Steganography object. This constructor is used for embedding purposes.
          * 
          * @param msg 
          * @param key 
@@ -43,7 +151,7 @@ class Steganography{
         Steganography(std::string msg, std::string key, std::string img_path);
 
         /**
-         * @brief Construct a new Steganography object.
+         * @brief Construct a new Steganography object. This constructor is used for extraction purposes.
          * 
          * @param key 
          * @param img 
@@ -55,12 +163,6 @@ class Steganography{
          * 
          */
         Steganography(const Steganography&) = delete;
-
-        /**
-         * @brief Loads image from the path given (img_path_).
-         * 
-         */
-        void LoadImage();
 
         /**
          * @brief Embeds the secret information into cover image and returns the path/name of stego image.
--- src/steganography.cpp
+++ src/steganography.cpp
@@ -5,7 +5,7 @@
  *        Class definition implementation in "../include/steganography.hpp"
  *        header file.
  * @version 0.1
- * @date 2020-04-01
+ * @date 2020-04-07
  * 
  * @copyright Copyright (c) 2020
  * 
@@ -14,27 +14,209 @@
 //#include <string>
 //#include <opencv2/imgcodecs.hpp>
 #include "../include/steganography.hpp"
-#include  "../include/convert.hpp"
+#include "../include/convert.hpp"
+#include "../third-party/sha256/include/sha256.hpp"
 
 Steganography::Steganography(std::string msg, std::string key, std::string image_path): secret_info_(msg), key_(key), img_path_(image_path){ }
 
 Steganography::Steganography(std::string key, std::string image_path): key_(key), img_path_(image_path){ }
 
 void Steganography::LoadImage(){
-    img_ = cv::imread(img_path_, cv::IMREAD_COLOR);
+    img_ = cv::imread(img_path_, cv::IMREAD_COLOR); // [B, G, R]
     if(img_.empty())
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); // failed to open image.
 }
 
-std::string Steganography::Extraction(){
+void Steganography::SaveStegoImage(std::string stego_img_path){
+    cv::imwrite(stego_img_path, img_);
+}
+
+std::string Steganography::HiddenBits(size_t size){
+    return HexadecimalToBinary(std::to_string(size));
+}
+
+bool Steganography::IfStorable(std::string number_of_bits_hidden)const{
+    if(img_.rows >= 10){
+        if(5 * img_.cols >= number_of_bits_hidden.length()){
+            if(5 * img_.cols >= key_hash_.length()){
+                if(img_.rows - 10 * img_.cols >= secret_info_.length()) return true;
+                return false;
+            }
+            return false;
+        }
+        return false;
+    }
+    return false;
+}
+
+bool Steganography::IsEven(unsigned char color_shade)const{
+    return color_shade % 2 == 0 ? true :false;
+}
+
+bool Steganography::IsOdd(unsigned char color_shade)const{
+    return color_shade % 2 == 1 ? true : false;
+}
+
+bool Steganography::XOR(char secret_bit, bool lsb)const{
+    switch(secret_bit){
+        case '0':
+            switch(lsb){
+                case false: return false;
+                case true:  return true;
+            }
+        case '1':
+            switch(lsb){
+                case false: return true;
+                case true:  return false;
+            }
+    }
+}
+
+void Steganography::CirculateKey (){
+    auto iterator = key_.begin();
+    key_.push_back(*iterator);
+    key_.erase(0, 1);
+}
+
+cv::Vec3b Steganography::EmbedInBlue(cv::Vec3b pixel, size_t index){
+    if(IsEven(secret_info_[index]) && IsEven(pixel[0]));  // if both bit to embed and blue lsb are 0 do nothing.
+    else if(IsEven(secret_info_[index]) && IsOdd(pixel[0]))  pixel[0] = pixel[0] - 1;    // if bit to embed is 0 and blue lsb is 1 change blue lsb to 0.
+    else if(IsOdd(secret_info_[index]) && IsEven(pixel[0]))  pixel[0] = pixel[0] + 1;    // if bit to embed is 1 and blue lsb is 0 change blue lsb to 1.
+    else;   // if both bit to embed and blue lsb are 1 do nothing.
+    return pixel;
+}
+
+cv::Vec3b Steganography::EmbedInGreen(cv::Vec3b pixel, size_t index){
+    if(IsEven(secret_info_[index]) && IsEven(pixel[1]));  // if both bit to embed and green lsb are 0 do nothing.
+    else if(IsEven(secret_info_[index]) && IsOdd(pixel[1]))  pixel[1] = pixel[1] - 1;    // if bit to embed is 0 and green lsb is 1 change green lsb to 0.
+    else if(IsOdd(secret_info_[index]) && IsEven(pixel[1]))  pixel[1] = pixel[1] + 1;    // if bit to embed is 1 and green lsb is 0 change green lsb to 1.
+    else;   // if both bit to embed and green lsb are 1 do nothing.
+    return pixel;
+}
+
+cv::Vec3b Steganography::EmbedInRed(cv::Vec3b pixel, size_t index){
+    if(IsEven(secret_info_[index]) && IsEven(pixel[2]));  // if both bit to embed and red lsb are 0 do nothing.
+    else if(IsEven(secret_info_[index]) && IsOdd(pixel[2]))  pixel[2] = pixel[2] - 1;    // if bit to embed is 0 and red lsb is 1 change red lsb to 0.
+    else if(IsOdd(secret_info_[index]) && IsEven(pixel[2]))  pixel[2] = pixel[2] + 1;    // if bit to embed is 1 and red lsb is 0 change red lsb to 1.
+    else;   // if both bit to embed and red lsb are 1 do nothing.
+    return pixel;
+}
+
+void Steganography::EmbedLength(std::string number_of_bits_hidden){
+    for(size_t i = 0, count = 0; i < 5; i++)
+        for(size_t j = 0; j < img_.cols; j++, count++){
+            cv::Vec3b pixel = img_.at<cv::Vec3b>(cv::Point(i, j));  // get pixel.
+            if(count < number_of_bits_hidden.length()){
+                if(IsOdd(pixel[1]))   pixel[1] = pixel[1] - 1;  // green lsb to 0.
+                if(IsEven(number_of_bits_hidden[count]) && IsEven(pixel[2])); // if both bit to embed and red lsb are 0 do nothing.
+                else if(IsEven(number_of_bits_hidden[count]) && IsOdd(pixel[2]))  pixel[2] = pixel[2] - 1;    // if bit to embed is 0 and red lsb is 1 change red lsb to 0.
+                else if(IsOdd(number_of_bits_hidden[count]) && IsEven(pixel[2]))  pixel[2] = pixel[2] + 1;    // if bit to embed is 1 and red lsb is 0 change red lsb to 1.
+                else;   // if both bit to embed and red lsb are 1 do nothing.
+                img_.at<cv::Vec3b>(cv::Point(i, j)) = pixel;  // save changes to pixel back to the image.
+            }
+            else{
+                if(IsEven(pixel[1]))  pixel[1] + 1; // green lsb to 1.
+                img_.at<cv::Vec3b>(cv::Point(i, j)) = pixel;  // save changes to pixel back to the image.
+                return;
+            }
+        }
+}
+
+void Steganography::EmbedKeyHash(){
+    for(size_t i = 5, count = 0; i < 10; i++)
+        for(size_t j = 0; j < img_.cols; j++, count++){
+            cv::Vec3b pixel = img_.at<cv::Vec3b>(cv::Point(i, j));    // get pixel.
+            if(count < key_hash_.length()){
+                if(IsEven(key_hash_[count]) && IsEven(pixel[0]));  // if both bit to embed and blue lsb are 0 do nothing.
+                else if(IsEven(key_hash_[count]) && IsOdd(pixel[0]))  pixel[0] = pixel[0] - 1;    // if bit to embed is 0 and blue lsb is 1 change blue lsb to 0.
+                else if(IsOdd(key_hash_[count]) && IsEven(pixel[0]))  pixel[0] = pixel[0] + 1;    // if bit to embed is 1 and blue lsb is 0 change blue lsb to 1.
+                else;   // if both bit to embed and blue lsb are 1 do nothing.
+                img_.at<cv::Vec3b>(cv::Point(i, j)) = pixel;  // save changes to pixel back to the image.
+            }
+            else return;
+        }
+}
+
+void Steganography::EmbedSecretInfo(){
+    for(size_t i = 10, count = 0; i < img_.rows; i++){
+        for(size_t j = 0; j < img_.cols; j++, count++){
+            if(count < secret_info_.length()){
+                cv::Vec3b pixel = img_.at<cv::Vec3b>(cv::Point(i, j));    //get pixel.
+                switch(j % 3){
+                    /**
+                     * @brief Case 0: Use blue lsb for XOR. if 1 embed secret information in red lsb else embed in green lsb.
+                     * 
+                     */
+                    case 0:
+                        if(XOR(key_[0], IsOdd(pixel[0])))
+                            img_.at<cv::Vec3b>(cv::Point(i, j)) = EmbedInRed(pixel, count);                        
+                        else
+                            img_.at<cv::Vec3b>(cv::Point(i, j)) = EmbedInGreen(pixel, count);
+                        break;
+
+                    /**
+                     * @brief Case 1: Use green lsb for XOR. if 1 embed secret information in blue lsb else embed in red lsb.
+                     * 
+                     */
+                    case 1:
+                        if(XOR(key_[0], IsOdd(pixel[1])))
+                            img_.at<cv::Vec3b>(cv::Point(i, j)) = EmbedInBlue(pixel, count);                        
+                        else
+                            img_.at<cv::Vec3b>(cv::Point(i, j)) = EmbedInRed(pixel, count);
+                        break;
+
+                    /**
+                     * @brief Case 2: Use red lsb for XOR. if 1 embed secret information in green lsb else embed in blue lsb.
+                     * 
+                     */
+                    case 2:
+                        if(XOR(key_[0], IsOdd(pixel[2])))
+                            img_.at<cv::Vec3b>(cv::Point(i, j)) = EmbedInGreen(pixel, count);                        
+                        else
+                            img_.at<cv::Vec3b>(cv::Point(i, j)) = EmbedInBlue(pixel, count);
+                        break;
+                }
+            }
+            else return;
+        }
+    }
+}
+
+std::string Steganography::Embedding(){
     LoadImage();
     
     /**
-     * @brief Secret info encryption to be left for last, so the following two line just converts secret info to binary form.
+     * @brief Secret info encryption to be left for last, so the following two line just converts secret infomation to binary form.
      * 
      */
     secret_info_ = TextToHexadecimal(secret_info_);
     secret_info_ = HexadecimalToBinary(secret_info_);
 
+    /**
+     * @brief Key hashing using sha256 and then convert to binary form.
+     * 
+     */
+    key_hash_ = sha256(key_);
+    key_hash_ = HexadecimalToBinary(key_hash_);
+
+    /**
+     * @brief Change original key to bits.
+     * 
+     */
+    key_ = TextToHexadecimal(key_);
+    key_ = HexadecimalToBinary(key_);
+
+    std::string number_of_bits_hidden = HiddenBits(secret_info_.length());
+
+    if(!IfStorable(number_of_bits_hidden))
+        exit(EXIT_FAILURE);  // image is small.
     
+    EmbedLength(number_of_bits_hidden);
+    EmbedKeyHash();
+    EmbedSecretInfo();
+
+    std::string stego_img_path = "../scrum-board/Stego_1. 2020-03-11_20:23.jpeg";
+    SaveStegoImage(stego_img_path);
+
+    return stego_img_path;
 }
